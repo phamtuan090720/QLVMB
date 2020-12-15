7d01c6753e3eec55443d97e12ebe404e6f171b97
Phạm Tuân <52484769+phamtuan090720@users.noreply.github.com>
2020-12-15 17:36:00 +0700

    done

---
+++ Dump20201215/qlbanvemaybay_sanbay.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: qlbanvemaybay
+-- ------------------------------------------------------
+-- Server version	8.0.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `sanbay`
+--
+
+LOCK TABLES `sanbay` WRITE;
+/*!40000 ALTER TABLE `sanbay` DISABLE KEYS */;
+INSERT INTO `sanbay` VALUES (1,'Sân bay Quốc tế Cần Thơ',10),(2,'Sân bay Quốc tế Đà Nẵng	',4),(3,'Sân bay Quốc tế Cát Bi – Hải Phòng',3),(4,'Sân bay Quốc tế Nội Bài – Hà Nội	',2),(5,'Sân bay Quốc tế Tân Sơn Nhất	',1),(6,'Sân bay Quốc tế Cam Ranhq	',7),(7,'Sân bay Quốc tế Phú Quốc	',6),(8,'Sân bay Quốc tế Vinh – Nghệ An	',9),(9,'Sân bay Quốc tế Phú Bài – Huế',8),(10,'Sân Bay Liên Khương',5);
+/*!40000 ALTER TABLE `sanbay` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-12-15 17:32:21
